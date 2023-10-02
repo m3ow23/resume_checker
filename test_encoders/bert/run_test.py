f67759d0268c17b3a1168a149a6cc34a7db8ff61
Nathanael Memis <nathanaelmemis@gmail.com>
2023-10-02 10:49:50 +0800

    Pushed from LAPTOP-APUS673V

---
+++ test_encoders/bert/run_test.py
@@ -0,0 +1,12 @@
+import os
+from time import sleep
+
+import methods.s2s_slavepe_maxcs
+
+# sleep(600)
+
+import methods.s2s_slmaxpe_avecs
+
+os.system("start save_repo.bat")
+
+# os.system("shutdown /s /t 1")
